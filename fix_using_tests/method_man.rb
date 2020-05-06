637cb460d87920d6922d27dbadeddf5178e78309
Kelly Blackwell <kellylenorablackwell@gmail.com>
2020-05-06 21:08:38 +0000

    Automatically backed up by Learn

--- fix_using_tests/method_man.rb
+++ fix_using_tests/method_man.rb
@@ -1,13 +1,13 @@
 require 'pry'
 
-def start_game(player1,player2)
+def start_game
   player1 = "rocky"
   player2 = "bullwinkle"
   puts "Hello #{player1} and #{player2}"
   binding.pry
 end 
 
-start_game(rocky,bullwinkle)
+start_game(
 
 def play_game(player2)
   puts "#{player1} is better than #{player2}"
