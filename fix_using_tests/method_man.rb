990ae0c59bf8a00af35a5e881fe93d944d0ae89b
Kelly Blackwell <kellylenorablackwell@gmail.com>
2020-05-06 21:01:11 +0000

    Automatically backed up by Learn

--- fix_using_tests/method_man.rb
+++ fix_using_tests/method_man.rb
@@ -4,13 +4,9 @@ def start_game(player1,player2)
   puts "Hello #{player1} and #{player2}"
 end 
 
-player1(sheldon)
-player2(alan)
-
-start_game
+start_game(rocky,bullwinkle)
 
 def play_game(player2)
   puts "#{player1} is better than #{player2}"
 end
 
-start_game(sheldon,alan)
