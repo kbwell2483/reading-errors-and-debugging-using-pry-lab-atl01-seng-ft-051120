f5458977535adc3fd75b27a802f36b47be80d6a5
Kelly Blackwell <kellylenorablackwell@gmail.com>
2020-05-06 21:07:43 +0000

    Automatically backed up by Learn

--- fix_using_tests/method_man.rb
+++ fix_using_tests/method_man.rb
@@ -1,8 +1,8 @@
 require 'pry'
 
 def start_game(player1,player2)
-  player1 = rocky
-  player2 = bullwinkle
+  player1 = "rocky"
+  player2 = "bullwinkle"
   puts "Hello #{player1} and #{player2}"
   binding.pry
 end 
